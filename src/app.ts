b8a253b0ef63e1791253da30a669f6fda291d391
Scott Nesham <scott.nesham@asynchrony.com>
2018-01-17 19:21:54 -0600

    add fromStore to the application and set the initialState

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
+import * as fromStore from './store';
+
 import { renderTodos } from './utils';
 
 const input = document.querySelector('input') as HTMLInputElement;
 const button = document.querySelector('button') as HTMLButtonElement;
 const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
 const todoList = document.querySelector('.todos') as HTMLLIElement;
+
+const store = new fromStore.store({}, {
+  todos: [{ label: 'Eat pizza', complete: false}]
+});
 
 button.addEventListener(
   'click',
---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+export class store {
+  private subscribers: Function[];
+  private reducers: { [key: string]: Function };
+  private state: { [key: string]: any };
+
+  constructor(reducers = {}, initialState = {}) {
+    this.state = initialState;
+  }
+
+  get value() {
+    return this.state;
+  }
+};
